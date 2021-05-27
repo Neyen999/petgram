8db1e97c110557194e49c72ce277f147a861cc2d
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-05-27 15:18:25 -0300

    Creando ListOfCategories y agregando GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { Category } from './components/Category'
+import { ListOfCategories } from './components/ListOfCategories'
+import { GlobalStyle } from './GlobalStyles'
 
 export const App = () => {
   return (
-
-    <Category />
+    <>
+      <GlobalStyle />
+      <ListOfCategories />
+    </>
   )
 }
---
+++ src/components/ListOfCategories/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Category } from '../Category'
+
+import { List, Item } from './styles'
+
+export const ListOfCategories = () => {
+  return (
+    <List>
+      {
+        // eslint-disable-next-line react/jsx-key
+        [1, 2].map(category => <Item key={category}><Category /></Item>)
+      }
+    </List>
+  )
+}
