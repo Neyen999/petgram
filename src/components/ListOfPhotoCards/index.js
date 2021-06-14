85999f9c28fdc98837df734e62eb54556a5b812d
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-06-14 11:47:04 -0300

    Utilizando GraphQl y React Apollo

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
-import { photos } from '../../../api/db.json'
+// import { photos } from '../../../api/db.json'
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+const getPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`
 
 export const ListOfPhotoCards = () => {
+  const { loading, error, data } = useQuery(getPhotos)
+  if (loading) return <h2>Loading...</h2>
+  if (error) return <h2>Error!</h2>
   return (
     <ul>
-      {photos.map(photo => <PhotoCard key={photo.id} id={photo.id} src={photo.src} />)}
+      {data.photos.map(photo => <PhotoCard key={photo.id} id={photo.id} src={photo.src} />)}
     </ul>
   )
 }
