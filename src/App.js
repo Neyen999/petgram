5568ba16753d78f2881eea2be089ba16fba3671f
Neyen Ezequiel Marinelli <neyen_gt@hotmail.com>
2021-06-14 23:56:02 -0300

    Usando render props para recuperar una foto

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './styles/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
 import { Logo } from './components/Logo'
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 
 export const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+  console.log(detailId)
   return (
     <>
       <GlobalStyle />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCards categoryId={2} />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategories />
+            <ListOfPhotoCards categoryId={2} />
+            </>
+      }
     </>
   )
 }
