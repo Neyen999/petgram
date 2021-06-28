64494b3a1f2969b171ff7da4324916e5c772c53c
Neyen Marinelli <neyen_gt@hotmail.com>
2021-06-28 12:32:26 -0300

    Utilizando las render props para simular la autenticación de los usuarios y mostrar diferentes rutas si lo estan o no

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Router } from '@reach/router'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,21 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+          isAuth ?
+          <Router>
+            <Favs path="/favs"/>
+            <User path="/users"/>
+          </Router>
+          :
+          <Router>
+            <NotRegisteredUser path="/favs"/>
+            <NotRegisteredUser path="/users"/>
+          </Router>
+        }    
+      </UserLogged>
       <NavBar />
     </>
   )
